c5ca9cbeac9e3eddce2138450109ffd6cc2a2ae4
leon <miao1007@gmail.com>
2014-11-18 20:39:33 +0800

    add palette

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Feature
 ######NavigationDrawer Animation
 ![Image](NavigationDrawer.gif)
 
-######RecyclerView Anmation
+######RecyclerView Anmation&&Endless RecyclerView
 ![Image](recyclerview-anim.gif)
 
 Build Environment
@@ -34,11 +34,13 @@ Refferences
 1. [NavigationDrawer Guide](http://blog.mosil.biz/2014/10/navigation-drawer-on-toolbar/)
 2. [Recyclerview Layout Animation](https://github.com/mikepenz/Android-LollipopShowcase)
 3. [Lollipop Themes and Style](http://antonioleiva.com/material-design-everywhere/)
+4. [**More** Lollipop ShowCase](https://github.com/lightSky/MaterialDesignCenter)
 
 TODO
 -------------
 1. I will add more animation and V7-widget in the future.
 2. `makeSceneTransitionAnimation` between Activities is not support in pre-Lollipop device. I will try in another way
+
 
 Developed By
 -------------
--- app/src/main/java/com/github/miao1007/myapplication/DetailedActivity.java
+++ app/src/main/java/com/github/miao1007/myapplication/DetailedActivity.java
@@ -1,13 +1,18 @@
 package com.github.miao1007.myapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 
 public class DetailedActivity extends ActionBarActivity {
@@ -24,7 +29,6 @@ public class DetailedActivity extends ActionBarActivity {
         if (mToolbar != null) {
             setSupportActionBar(mToolbar);
             mToolbar.inflateMenu(R.menu.menu_detailed);
-            mToolbar.inflateMenu(R.menu.menu_detailed);
             // Set Navigation Toggle
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -32,12 +36,32 @@ public class DetailedActivity extends ActionBarActivity {
             //throw new NullPointerException("Toolbar must be <include> in activity's layout!");
         }
         mImageview = (ImageView) findViewById(R.id.imageView_detail);
+
+        //Picasso target get bitmap
+        Target target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+
+                //You can get 400x200 showcase pictures at http://lorempixel.com/
+                mImageview.setImageBitmap(bitmap);
+                Palette palette = Palette.generate(bitmap);
+                mToolbar.setBackgroundColor(palette.getDarkMutedColor(Color.RED));
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+                mImageview.setImageResource(R.drawable.lorempixel);
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                mImageview.setImageResource(R.drawable.lorempixel);
+            }
+        };
         Picasso.with(this)
                 .load(getIntent().getStringExtra(EXTRA_IMAGE))
-                //You can get 400x200 showcase pictures at http://lorempixel.com/
-                .error(R.drawable.lorempixel)
-                .placeholder(R.drawable.lorempixel)
-                .into(mImageview);
+                .into(target);
+
     }
 
 
